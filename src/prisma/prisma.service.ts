c9d78214c9affdbe0b1c0486c2739786a1f7b786
Darkpool645 <angel_camargo@outlook.com>
2026-08-11 21:48:01 -0600

    fix(prisma): add pg driver adapter required by Prisma 7 client

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,18 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from "../../generated/prisma/client";
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-    async onModuleInit() {
-        await this.$connect();
-    }
+  constructor() {
+    super({
+      adapter: new PrismaPg({
+        connectionString: process.env.DATABASE_URL,
+      }),
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
 }
